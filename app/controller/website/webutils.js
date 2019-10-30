0d9aab20a35e88f48bef49b75586c1fc5c5569d9
codeliling <maximuslee@126.com>
2019-10-30 11:12:21 +0800

    Update webutils.js

--- app/controller/website/webutils.js
+++ app/controller/website/webutils.js
@@ -6,7 +6,7 @@ class WebutilsController extends BaseController{
   async getCaptcha(){
     let codeConfig = {
         size: 5,// 验证码长度
-        ignoreChars: '0o1i', // 验证码字符中排除 0o1i
+        ignoreChars: '0oO1ilLI', // 验证码字符中排除 0o1i
         noise: 2, // 干扰线条的数量
         height: 14
     }
